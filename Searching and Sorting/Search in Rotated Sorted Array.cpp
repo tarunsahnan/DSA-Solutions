834b194a37a953b9ddaec2c6b9d3a5cddc1c2561
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-01-26 22:53:27 +0530

    Search in Rotated Sorted Array

---
+++ Searching and Sorting/Search in Rotated Sorted Array.cpp
@@ -0,0 +1,31 @@
+//Question Link -->https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+int helper(vector<int>& nums,int target,int start, int end){
+        
+        if(start>end)        
+        return -1;
+        
+        int mid=(start+end)/2;
+        if(nums[mid]==target)
+        return mid;
+    
+        
+
+    if(nums[start] <= nums[mid]){
+        //means first half is sorted
+
+        if(nums[start] <= target and target<= nums[mid])
+            return helper(nums,target,start,mid);
+        else
+            return helper(nums,target,mid+1,end);}
+
+    else{
+        
+        //means second half is sorted
+        if(nums[mid] <= target and target<= nums[end])
+            return helper(nums,target,mid+1,end);
+        else
+            return helper(nums,target,start,mid);
+
+    }
+    }
