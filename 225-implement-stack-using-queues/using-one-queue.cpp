714fee44ad1f0e0d3d50718696b5005173452770
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-01-30 22:52:48 +0530

    Create using-one-queue.cpp

---
+++ 225-implement-stack-using-queues/using-one-queue.cpp
@@ -0,0 +1,43 @@
+//Question Link --> https://leetcode.com/problems/implement-stack-using-queues/
+
+class MyStack {
+public:
+    queue<int> q1;
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        
+        q1.push(x);
+        
+        for(int i=0;i<q1.size()-1;i++){
+            q1.push(q1.front());
+            q1.pop();
+        }
+        
+    }
+    
+    int pop() {
+        int temp=q1.front();
+        q1.pop();
+        return temp;
+    }
+    
+    int top() {
+        return q1.front();
+    }
+    
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
