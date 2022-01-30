d3e4fc60638539ae75593cdfca35c931999df6fe
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-01-30 12:43:12 +0530

    Create Deque.cpp

---
+++ 239-sliding-window-maximum/Deque.cpp
@@ -0,0 +1,27 @@
+//Question Link -->
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        
+        vector<int> res;
+        deque<int> dq;
+        
+        for(int i=0;i<nums.size();i++){
+            
+            if( !dq.empty() && (dq.front() == i-k))
+               dq.pop_front();
+            
+            while(!dq.empty() and nums[dq.back()] < nums[i] )
+                dq.pop_back();
+                
+            
+             dq.push_back(i);
+                     
+            if(i >= k-1){
+                res.push_back(nums[dq.front()]);
+            }
+        }
+        return res;
+    }
+};
