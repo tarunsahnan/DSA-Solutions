723f675c2b291ae13d2704d65a954a5c9a33b953
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-01-31 13:34:15 +0530

    Create recursion.cpp

---
+++ 94-binary-tree-inorder-traversal/recursion.cpp
@@ -0,0 +1,38 @@
+//Question Link --> https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    
+    void helper(TreeNode* root,vector<int>& res){
+        
+        if(!root) return;
+            
+        helper(root->left,res);
+        
+        res.push_back(root->val);
+        
+        helper(root->right,res);
+        
+        
+        
+        
+    }
+    
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        helper(root,res);
+        return res;
+    }
+};
