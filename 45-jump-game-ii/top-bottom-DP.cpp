a84e7b097b3233cd65ed7a1503c7ec5b00565552
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-02-09 00:12:34 +0530

    Update top-bottom-DP.cpp

--- 45-jump-game-ii/top-bottom-DP.cpp
+++ 45-jump-game-ii/top-bottom-DP.cpp
@@ -3,19 +3,19 @@
 class Solution {
 public:
     
-    int dp[10005][1005];  //currpos,jumps
+    int dp[10005];  //currpos
     
     int rec(vector<int>& nums, int curr_pos){
         
         if(curr_pos >= nums.size()-1 ) return 0;
         
-        if(dp[curr_pos][nums[curr_pos]] != 0) return dp[curr_pos][nums[curr_pos]];
+        if(dp[curr_pos] != 0) return dp[curr_pos];
         
         int min_jumps = 10000;
         for(int i=1;i<=nums[curr_pos];i++)
             min_jumps = min(min_jumps, 1 + rec(nums,curr_pos+i));
         
-        return dp[curr_pos][nums[curr_pos]] = min_jumps;
+        return dp[curr_pos] = min_jumps;
         
     }
     
