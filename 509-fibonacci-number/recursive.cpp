4865a3d9c12cf7039df5e71be22c49aebebdd0e5
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-02-08 07:53:47 +0530

    Create recursive.cpp

---
+++ 509-fibonacci-number/recursive.cpp
@@ -0,0 +1,11 @@
+//Question Link --> https://leetcode.com/problems/fibonacci-number/
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n==0 || n==1)
+            return n;
+        
+        return fib(n-1)+fib(n-2);
+    }
+};
