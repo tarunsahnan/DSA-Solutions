aea0fe504a11b2284a3902b85d3d0fbc938f0d55
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-02-15 22:59:31 +0530

    Create xor.cpp

---
+++ 136-single-number/xor.cpp
@@ -0,0 +1,14 @@
+//Question Link --> https://leetcode.com/problems/single-number/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans=0;
+        
+        for(auto i:nums)
+            ans^= i;
+        
+        return ans;
+    }
+};
