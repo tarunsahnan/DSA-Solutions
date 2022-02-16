f67a8c83e51fd502fc21334b4ccab8b41143d2f2
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-02-17 00:53:12 +0530

    Create two-pointer.cpp

---
+++ 167-two-sum-II-input-array-is-sorted/two-pointer.cpp
@@ -0,0 +1,18 @@
+//Question Link --> https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+     
+        int i1=0,i2=nums.size()-1;
+        
+        while(i1 <=i2 ){
+            
+            if(nums[i1]+nums[i2] == target) return {i1+1,i2+1};
+            else if(nums[i1]+nums[i2] > target) i2--;
+            else i1++;
+            
+        }
+        return {};
+    }
+};
