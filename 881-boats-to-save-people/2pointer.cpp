92b7936bbd7d5e2f08b64dec897e44bb0ec0ebc8
tarunsahnan <tarunsahnan98@gmail.com>
2022-03-24 17:33:14 +0530

    solution added

---
+++ 881-boats-to-save-people/2pointer.cpp
@@ -0,0 +1,23 @@
+//OJ --> https://leetcode.com/problems/boats-to-save-people/
+
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        
+        int left=0,right=people.size()-1,cnt=0;
+        
+        while(left<=right){
+            
+            if(people[left]+people[right] <= limit){
+                left++,right--,cnt++;
+            }
+            else
+                right--,cnt++;   
+        }
+        
+        return cnt;
+        
+    }
+};
+
