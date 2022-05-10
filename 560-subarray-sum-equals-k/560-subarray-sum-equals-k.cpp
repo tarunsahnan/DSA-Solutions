f58d81ce923e09b47b751e454bd42473c5260666
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-05-11 00:31:06 +0530

    Time: 69 ms (94.59%), Space: 36.2 MB (44.36%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int prefix[nums.size()];
+        prefix[0]=nums[0];
+        
+        int n=nums.size(),res=0;        
+        for(int i=1;i<n;i++){
+            prefix[i] = prefix[i-1] + nums[i];
+        }
+        
+        unordered_map<int,int> hash;
+
+        for(int i=0;i<n;i++){
+         
+            if(prefix[i] == k)
+                res++;
+            
+            int target=prefix[i]-k;
+            
+            if(hash.find(target) != hash.end()){
+                res+= hash[target];
+            }
+            
+            hash[prefix[i]]++;;
+        }
+        return res;
+    }
+};
