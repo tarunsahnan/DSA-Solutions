130e98500b559f2f69c95eedda551ce1ce704fe6
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-05-19 07:08:12 +0530

    Added solution - LeetHub

---
+++ 16.                         Vertical Width of a Binary Tree - GFG/16.-vertical-width-of-a-binary-tree.cpp
@@ -0,0 +1,160 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+
+// Function to Build Tree
+Node *buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;)
+        ip.push_back(str);
+
+    // Create the root of the tree
+    Node *root = new Node(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node *> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node *currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size())
+            break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+// your task is to complete this functionh
+// function should return the width of the tree
+int verticalWidth(Node* root);
+
+
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Structure of node of binary tree is as follows
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+*/
+
+
+//Function to find the vertical width of a Binary Tree.
+int mmax=0,mmin=0;
+
+void lengthUtil(Node* root, int &maximum,
+                int &minimum, int curr=0)
+{
+    if (root == NULL)
+        return;
+ 
+    // traverse left
+    lengthUtil(root->left, maximum,
+               minimum, curr - 1);
+ 
+    // if curr is decrease then get
+    // value in minimum
+    if (minimum > curr)
+        minimum = curr;
+ 
+    // if curr is increase then get
+    // value in maximum
+    if (maximum < curr)
+        maximum = curr;
+ 
+ 
+    // traverse right
+    lengthUtil(root->right, maximum,
+               minimum,  curr + 1);
+ 
+}
+
+int verticalWidth(Node* root){
+    
+    if(root == NULL) return 0;
+    int minn=0,maxx=0;
+    lengthUtil(root,maxx,minn,0);
+    return maxx-minn+1;
+}
+
+// { Driver Code Starts.
+
+int main() {
+    int tc;
+    scanf("%d ", &tc);
+    while (tc--) {
+        string str;
+        getline(cin, str);
+        Node *root = buildTree(str);
+        
+        cout << verticalWidth(root) << "\n";
+    }
+
+
+    return 0;
+}
+  // } Driver Code Ends
