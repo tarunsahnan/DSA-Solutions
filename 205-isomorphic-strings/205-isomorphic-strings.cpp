82e5e0d547d5d95e55872e66c458b74d86eb22f4
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-07-01 11:30:57 +0530

    Time: 7 ms (77.94%), Space: 7 MB (46.45%) - LeetHub

---
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        
+        int arr1[256]={0},arr2[256]={0};
+        
+        for(int i=0;i<s.size();i++){
+            if(arr1[s[i]] != arr2[t[i]]) return false;
+            arr1[s[i]]=i+1;
+            arr2[t[i]]=i+1;
+
+        }
+        return true;
+        
+    }
+};
