7a3f9e609cc39124db469d9483e76ecb6b4b3029
Tarun Sahnan <tarunsahnan98@gmail.com>
2022-07-02 17:51:27 +0530

    Time: 0 ms (100.00%), Space: 6 MB (73.00%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int dp[101][101];
+    
+    int rec(int m, int n)
+    {
+        
+        if(m==0 || n==0)
+            return 1;
+        
+        if(dp[m][n] != 0) return dp[m][n];
+        
+        int ans=rec(m-1,n) + rec(m,n-1);
+        dp[m][n] =ans;
+        return ans;
+        
+    }
+    
+    
+    int uniquePaths(int m, int n) {        
+    return rec(m-1,n-1);
+}
+};
